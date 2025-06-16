b3460b9a50c184e26ba6f8007cb8eb67320b93d3
Juan_Es <juanesgsw@gmail.com>
2025-06-16 13:02:24 +0200

    feat: funciones agregadas (mostrar la lista de tareas) fix: se ha arreglado todo error que no permitia que el codigo funcione

--- src/TaskGestor.java
+++ src/TaskGestor.java
@@ -3,58 +3,91 @@ import java.util.Scanner;
 public class TaskGestor {
     Scanner s = new Scanner(System.in);
 
-    private String titleTask;
-    private String taskContent;
+    private int numTask;
+    private String task;
+    private int taskPriority;
+    private String[] arrTask;
 
-    public String getTitleTask(){
-        return this.titleTask;
+    public String[] getArrTask(){
+        return this.arrTask;
     }
 
-    public void setTitleTask(String titleTask){
-        this.titleTask = titleTask;
+    public String getTask(){
+        return this.task;
     }
 
-    public String getTaskContent(){
-        return this.taskContent;
+    public void setTask(String task){
+        this.task = task;
     }
 
-    public void setTaskContent(String taskContent){
-        this.taskContent = taskContent;
+    public int getNumTask(){
+        return this.numTask;
     }
 
-    public void showMenu(){
-        int opt = 0;
+    public void setNumTask(int numTask){
+        this.numTask = numTask;
+    }
+
+    public void taskMenu(){
+        int opt;
+
         do {
             System.out.println("Administrador de tareas\n");
-            System.out.println("1.) Agregar tarea");
-            System.out.println("2.) ver tareas");
-            System.out.println("3.) Salir del programa...");
-            System.out.print("\nIngrese una opcion valida: ");
-            opt = s.nextByte();
+            System.out.println("1.) agregar tarea");
+            System.out.println("2.) mostrar tareas");
+            System.out.println("3.) salir del programa...\n");
+            System.out.print("Ingrese una opcion valida: ");
+            opt = s.nextInt();
 
+            switch (opt){
+                case 1 -> {
+                    addTask();
+                }
 
-        }while(opt != 3);
+                case 2 -> {
+                    showTasks();
+                }
 
+                case 3 ->{
+                    System.out.println("Hasta luego...");
+                    System.exit(0);
+                }
+            }
+
+        }while (opt != 3);
     }
 
     public void addTask(){
-        String title;
-        String details;
-        System.out.print("Titulo: ");
-        title = s.nextLine();
-        System.out.println("detalles: ");
-        details = s.nextLine();
+        System.out.println("Ingrese el numero de tareas que agregara: ");
+        int numTask = s.nextInt();
+        s.nextLine();
+        setNumTask(numTask);
+        arrTask = new String[getNumTask()];
 
-        if (title == "" && details == ""){
-            System.out.println("Tarea vacia, no se ha agregado. ");
-        }else {
-            System.out.println("Tarea agregada exitosamente");
+        for (int i = 0; i < arrTask.length; i++){
+            System.out.print("Ingrese la tarea: ");
+            String task = s.nextLine();
+            setTask(task);
+
+            if (task.equals("")){
+                System.out.println("Tarea vacia. no se agregara");
+                i--;
+            }else {
+                arrTask[i] = getTask();
+            }
+        }
+        System.out.println("Tareas agregadas correctamente.");
+
+
+    }
+
+    public void showTasks(){
+        System.out.println("Lista de tareas");
+        int i = 1;
+        for (String tasks : getArrTask()){
+            System.out.println((i++) +". "+tasks);
+
         }
     }
-
-    public void showTask(){
-        addTask();
-    }
-
 
 }
